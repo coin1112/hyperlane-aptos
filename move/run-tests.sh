cefb42402bcc2427e203ea9fbb3e442aff98c937
coin1 <coinlover664@gmail.com>
2024-08-12 23:39:05 -0700

    added aptos conracts

---
+++ move/run-tests.sh
@@ -0,0 +1,7 @@
+./function.sh test_router
+./function.sh test_validator
+./function.sh test_isms
+./function.sh test_mailbox
+./function.sh test_igps
+./function.sh test_router
+./function.sh test_examples
